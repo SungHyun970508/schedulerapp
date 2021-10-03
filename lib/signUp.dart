baa701918fe007658dd908777f420ce68a5c44bf
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-10-03 19:55:57 +0900

    회원가입 페이지에 텍스트필드, 버튼 추가

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'main.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -6,10 +7,30 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(middle: Text("Sign up")),
-        child: Center(
-          child: CupertinoButton(
-              child: Text("Back"),
-              onPressed: () => Navigator.of(context).pop()),
-        ));
+        child: SafeArea(
+            child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: CupertinoTextField(
+                          clearButtonMode: OverlayVisibilityMode.editing,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                          ),
+                        )),
+                    CupertinoButton(
+                      child: Text("Sign Up"),
+                    ),
+                  ],
+                ))));
   }
 }
+
+// child: Center(
+//           child: CupertinoButton(
+//               child: Text("Back"),
+//               onPressed: () => Navigator.of(context).pop()),
+//         )
