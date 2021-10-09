5cdec9c4f16505902f7dabf0f4f343c35da63920
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-10-10 00:01:34 +0900

    signuptest페이지 ui 변경

--- lib/sign_up_test.dart
+++ lib/sign_up_test.dart
@@ -23,8 +23,15 @@ class SignUpTestPage extends StatefulWidget {
   _SignUpTestPage createState() => _SignUpTestPage();
 }
 
-Future<UserModel> createUser(String email, String group, int img,
-    String loginId, String nickName, String phoneNumber, String pwd, String userName) async{
+Future<UserModel> createUser(
+    String email,
+    int group,
+    String img,
+    String loginId,
+    String nickName,
+    String phoneNumber,
+    String pwd,
+    String userName) async {
   final String apiUrl = "https://api/v1/auth/sign-up";
 
   final response = await http.post(Uri.parse(apiUrl), body: {
@@ -38,17 +45,16 @@ Future<UserModel> createUser(String email, String group, int img,
     "userName": userName
   });
 
-  if(response.statusCode == 201){
+  if (response.statusCode == 201) {
     final String responseString = response.body;
 
     return userModelFromJson(responseString);
-  }else{
+  } else {
     return null;
   }
 }
 
 class _SignUpTestPage extends State<SignUpTestPage> {
-
   UserModel _user;
 
   final TextEditingController emailController = TextEditingController();
@@ -62,75 +68,105 @@ class _SignUpTestPage extends State<SignUpTestPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body:Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+            child: Column(
           children: <Widget>[
-
             TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter email here'
+              ),
               controller: emailController,
             ),
+            SizedBox(height: 10),
 
             TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter group integer here'
+              ),
               controller: groupController,
             ),
+            SizedBox(height: 10),
 
             TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter img string here'
+              ),
               controller: imgController,
             ),
+            SizedBox(height: 10),
 
             TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter loginId here'
+              ),
               controller: loginIdController,
             ),
+            SizedBox(height: 10),
 
             TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter nickName here'
+              ),
               controller: nickNameController,
             ),
+            SizedBox(height: 10),
 
             TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter phoneNumber here'
+              ),
               controller: phoneNumberController,
             ),
+            SizedBox(height: 10),
 
             TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter pwd here'
+              ),
               controller: pwdController,
             ),
+            SizedBox(height: 10),
 
             TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter userName here'
+              ),
               controller: userNameController,
             ),
+            SizedBox(height: 10),
 
-            SizedBox(height: 32,),
-
-            _user == null ? Container() :
-            Text("The user ${_user.email}, ${_user.loginId} is created successfully at time ${_user.userName}"),
-
+            _user == null
+                ? Container()
+                : Text(
+                    "The user ${_user.email}, ${_user.loginId} is created successfully at time ${_user.userName}"),
           ],
-        ),
+        )),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async{
+        onPressed: () async {
           final String email = emailController.text;
-          final String group = groupController.text;
-          final int img = int.parse(imgController.text);
+          final int group = int.parse(groupController.text);
+          final String img = imgController.text;
           final String loginId = loginIdController.text;
           final String nickName = nickNameController.text;
           final String phoneNumber = phoneNumberController.text;
           final String pwd = pwdController.text;
           final String userName = userNameController.text;
 
-          final UserModel user = await createUser(email, group, img, loginId, nickName, phoneNumber, pwd, userName);
+          final UserModel user = await createUser(
+              email, group, img, loginId, nickName, phoneNumber, pwd, userName);
 
           setState(() {
             _user = user;
           });
-
         },
         tooltip: 'Increment',
         child: Icon(Icons.add),
